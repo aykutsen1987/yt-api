aa353f79ab4588af3f40f776ed644165e542c2d4
aykutsen1987 <aykutsen1987@gmail.com>
2025-12-15 16:03:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 from search import search_music
+from stream import get_stream_url
+from download import download_audio
 
-
-app = FastAPI()
+app = FastAPI(title="Mp3DMeta Backend")
 
 @app.get("/")
 def root():
     return {"status": "Mp3DMeta backend running"}
 
 @app.get("/search")
-def search(q: str):
-    return search_music(q)
+async def search(q: str):
+    return await search_music(q)
 
 @app.get("/stream/{video_id}")
-def stream(video_id: str):
-    return stream_audio(video_id)
+async def stream(video_id: str):
+    data = await get_stream_url(video_id)
+    if not data:
+        return {"error": "Stream alınamadı"}
+    return data
 
 @app.get("/download/{video_id}")
-def download(video_id: str):
-    return download_audio(video_id)
+async def download(video_id: str):
+    return await download_audio(video_id)
