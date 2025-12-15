4f56fb5d704a0b27e17255e0d50d5edbadd56482
aykutsen1987 <aykutsen1987@gmail.com>
2025-12-15 15:50:47 +0300

    Create stream.py

---
+++ stream.py
@@ -0,0 +1,19 @@
+import yt_dlp
+import tempfile
+
+async def get_stream_url(video_id: str):
+    ydl_opts = {
+        "format": "bestaudio",
+        "quiet": True,
+        "nocheckcertificate": True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(
+                f"https://www.youtube.com/watch?v={video_id}",
+                download=False
+            )
+            return info["url"]
+        except:
+            return None
