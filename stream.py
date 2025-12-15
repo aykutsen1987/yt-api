dc34143033f8cb935b5683b02396e933ce7e67db
aykutsen1987 <aykutsen1987@gmail.com>
2025-12-15 16:09:02 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,18 +4,12 @@ async def get_stream_url(video_id: str):
     ydl_opts = {
         "format": "bestaudio",
         "quiet": True,
-        "nocheckcertificate": True,
+        "nocheckcertificate": True
     }
 
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(
-                f"https://www.youtube.com/watch?v={video_id}",
-                download=False,
-            )
-            return {
-                "streamUrl": info["url"],
-                "expiresIn": 600,
-            }
-    except Exception:
-        return None
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(
+            f"https://www.youtube.com/watch?v={video_id}",
+            download=False
+        )
+        return info.get("url")
